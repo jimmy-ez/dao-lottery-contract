bdda84955d55b8ae1124567b55d66da65634d425
jimmy-ez <s.pramuansin@gmail.com>
2022-08-25 21:21:42 +0700

    Edit openzeppelin version

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts@4.7.3/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Lotto is ERC20, Ownable {
     constructor() ERC20("Lotto", "LTT") {}
